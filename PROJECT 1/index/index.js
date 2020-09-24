86b8c472cb24e3727665282806b0bd4d35d7d4c5
Abhinav Verma <johnverma577@gmail.com>
2020-09-24 22:05:32 +0530

    final
    Done

---
+++ PROJECT 1/index/index.js
@@ -0,0 +1,17 @@
+
+/*function results() {
+
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+
+  document.write("Thanks for contacting")
+}*/
+
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
